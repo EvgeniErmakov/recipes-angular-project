970ce4ceaf9cc69c436f897ac04cc691156d19a1
eermakov <EVErmakov@lanit.ru>
2023-03-14 21:41:31 +0300

    Section8: 103.Closing the Dropdown From Anywhere

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,4 +1,4 @@
-import {Directive, HostBinding, HostListener} from "@angular/core";
+import {Directive, HostBinding, HostListener, ElementRef} from "@angular/core";
 
 @Directive({
   selector: '[appDropdown]'
@@ -6,7 +6,16 @@ import {Directive, HostBinding, HostListener} from "@angular/core";
 export class DropdownDirective {
   @HostBinding('class.open') isOpen = false;
 
+  constructor(private elRef: ElementRef) {}
+
+  /* Срабатывает когда мы нажимаем на выпадающий список
   @HostListener('click') toggleOpen(){
     this.isOpen = !this.isOpen;
+  }
+   */
+
+  // Закрывает список, когда мы нажимаем на любую область на экране
+  @HostListener('document:click', ['$event']) toggleOpenForDocument(event: Event) {
+    this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
   }
 }
